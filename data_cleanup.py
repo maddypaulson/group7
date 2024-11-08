1c8b73bc2a10c36b73494bc133327dad4f2da1ba
dbon0003 <153691572+dbon0003@users.noreply.github.com>
2024-11-07 18:03:12 -0800

    Add files via upload

--- data_cleanup.py
+++ data_cleanup.py
@@ -7,7 +7,7 @@ csv_file_path = '2019_dataset_en.csv'
 df = pd.read_csv(csv_file_path)
 
 # Drop the columns
-df = df.drop(labels=["P_SEX","P_AGE","P_PSN","P_USER","P_SAFE","C_VEHS"],axis=1)
+df = df.drop(labels=["P_SEX","P_AGE","P_PSN","P_USER","P_SAFE"],axis=1)
 
 # Remove rows where 'P_ID' column contains 'UU' or 'NN'
 df = df[~df['P_ID'].isin(['UU', 'NN'])]
@@ -15,14 +15,14 @@ df = df[~df['P_ID'].isin(['UU', 'NN'])]
 # Reset the index to make the rows sequential
 df.reset_index(drop=True, inplace=True)
 
-# Print the first 20 rows of the dataframe
-print(df.head(20))
-
 # Sum the number of unique numbers in C_CASE column
 unique_cases_count = df['C_CASE'].nunique()
 
 # Get a specific column from the dataframe
 cur_C_CASE = df['C_CASE'][0]
+new_df = pd.DataFrame(columns=["C_YEAR","C_MNTH","C_WDAY","C_HOUR","C_SEV","C_CONF","C_RCFG","C_WTHR","C_RSUR","C_RALN","C_TRAF","V_ID","V_TYPE","V_YEAR","P_ISEV","C_CASE"])
 
-
-
+gk = df.groupby(["C_CASE","V_ID"])
+for name, group in gk:
+    print(f"Group name: {name}")
+    print(group)
